0dc6217cf4c6cca93ff7f79d93f7aa175297cd9e
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-12 20:54:17 -0300

    Handle the initial input conditions

--- callrecord/api/resource.py
+++ callrecord/api/resource.py
@@ -6,13 +6,15 @@ class CallRecord(Resource):
 
     def get(self, phonenumber=None, month=None, year=None):
         if phonenumber:
-            return {'success': 'GET method is working!'}, 200
-        return {'error': 'Phone number is required!'}, 502
+            return {'success': 'GET method is working'}, 200
+        return {'error': 'Phone number is required'}, 400
 
     def post(self, phonenumber=None, month=None, year=None):
         if phonenumber or month or year:
-            return {"error": "no route is required"}, 502
+            return {"error": "no route is required"}, 400
+
         call_data = request.get_json()
         if call_data:
-            return call_data, 200
-        return {'success': 'POST method is working!'}, 200
+            return {'success': 'POST method is working'}, 200
+
+        return {'error': 'Call information is needed'}, 400
