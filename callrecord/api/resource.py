f147d4235750bb3157c591feb48a982967019c13
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-12 20:38:44 -0300

    Add all the parameters of the post function

--- callrecord/api/resource.py
+++ callrecord/api/resource.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from flask import request
 
 
 class CallRecord(Resource):
@@ -8,5 +9,10 @@ class CallRecord(Resource):
             return {'success': 'GET method is working!'}, 200
         return {'error': 'Phone number is required!'}, 502
 
-    def post(self):
+    def post(self, phonenumber=None, month=None, year=None):
+        if phonenumber or month or year:
+            return {"error": "no route is required"}, 502
+        call_data = request.get_json()
+        if call_data:
+            return call_data, 200
         return {'success': 'POST method is working!'}, 200
