c7c1ef43109e7560e0acb33f5aad54801fe74c1c
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-12 20:36:39 -0300

    Add all the parameters of the get function

--- callrecord/api/resource.py
+++ callrecord/api/resource.py
@@ -3,8 +3,10 @@ from flask_restful import Resource
 
 class CallRecord(Resource):
 
-    def get(self):
-        return {'success': 'GET method is working!'}, 200
+    def get(self, phonenumber=None, month=None, year=None):
+        if phonenumber:
+            return {'success': 'GET method is working!'}, 200
+        return {'error': 'Phone number is required!'}, 502
 
     def post(self):
         return {'success': 'POST method is working!'}, 200
