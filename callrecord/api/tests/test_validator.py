8106899515c951c5bdfee915b20493530160e56b
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-13 01:54:40 -0300

    Add extra data validation test

--- callrecord/api/tests/test_validator.py
+++ callrecord/api/tests/test_validator.py
@@ -39,3 +39,25 @@ def test_incomplete_call_data():
         "call_id": 71
     }
     assert call_data_validator(complete_end_record) == False
+
+
+def test_extra_call_data():
+    complete_start_record = {
+        "id": "123654789",
+        "type": "start",
+        "timestamp": "2017-12-12T15:07:13Z",
+        "call_id": 71,
+        "source": "84912345678",
+        "destination": "84917415658",
+        "something": "more text here"
+    }
+    assert call_data_validator(complete_start_record) == False
+
+    complete_end_record = {
+        "id": "123654789",
+        "type": "end",
+        "something": "more text here",
+        "timestamp": "2017-12-12T15:07:13Z",
+        "call_id": 71
+    }
+    assert call_data_validator(complete_end_record) == False
