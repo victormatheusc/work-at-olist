11c0b195c07a8477cda2c691cc6949d625241026
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-13 01:53:43 -0300

    Add complete data validation test

---
+++ callrecord/api/tests/test_validator.py
@@ -0,0 +1,23 @@
+import pytest
+
+from ..validator import call_data_validator
+
+
+def test_complete_call_data():
+    complete_start_record = {
+        "id": "123654789",
+        "type": "start",
+        "timestamp": "2017-12-12T15:07:13Z",
+        "call_id": 71,
+        "source": "84912345678",
+        "destination": "84917415658"
+    }
+    assert call_data_validator(complete_start_record) == True
+
+    complete_end_record = {
+        "id": "123654789",
+        "type": "end",
+        "timestamp": "2017-12-12T15:07:13Z",
+        "call_id": 71
+    }
+    assert call_data_validator(complete_end_record) == True
