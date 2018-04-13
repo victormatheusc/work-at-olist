9540041b8d01fe32601cd84b9416d671ecf85ec6
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-12 21:25:34 -0300

    Add the save data return into post method

--- callrecord/api/resource.py
+++ callrecord/api/resource.py
@@ -1,12 +1,15 @@
 from flask_restful import Resource
 from flask import request
 
+from .views import save_call_data
+
 
 class CallRecord(Resource):
 
     def get(self, phonenumber=None, month=None, year=None):
         if phonenumber:
             return {'success': 'GET method is working'}, 200
+        
         return {'error': 'Phone number is required'}, 400
 
     def post(self, phonenumber=None, month=None, year=None):
@@ -15,6 +18,6 @@ class CallRecord(Resource):
 
         call_data = request.get_json()
         if call_data:
-            return {'success': 'POST method is working'}, 200
+            return save_call_data(call_data)
 
         return {'error': 'Call information is needed'}, 400
