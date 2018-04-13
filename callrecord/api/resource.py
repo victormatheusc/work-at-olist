fe84ab03813e8dbeda8513001f1bfc7f81b5c583
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-12 21:46:36 -0300

    Add the bill return into get method

--- callrecord/api/resource.py
+++ callrecord/api/resource.py
@@ -1,14 +1,14 @@
 from flask_restful import Resource
 from flask import request
 
-from .views import save_call_data
+from .views import save_call_data, get_bill
 
 
 class CallRecord(Resource):
 
     def get(self, phonenumber=None, month=None, year=None):
         if phonenumber:
-            return {'success': 'GET method is working'}, 200
+            return get_bill(phonenumber)
         
         return {'error': 'Phone number is required'}, 400
 
